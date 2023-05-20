1ddbf4c24d939ecfe4be46d53d75b7578ae15e5b
Phan Tung <phanmanhtungbeo@gmail.com>
2023-05-20 08:23:39 +0200

    Add input-output columns

---
+++ mytest/p2.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+import sys
+
+
+class App:
+    def __init__(self, root, myoptions):
+        self.root = root
+        self.x_options = []  # Empty list for X options initially
+        self.y_options = []  # Empty list for Y options initially
+        self.selected_x = tk.StringVar()
+        self.selected_y = tk.StringVar()
+        self.create_widgets()
+        self.myoptions = myoptions
+
+    def create_widgets(self):
+        self.option_frame = ttk.Frame(self.root)
+        self.option_frame.pack(padx=10, pady=10)
+
+        self.x_label = ttk.Label(self.option_frame, text="X:")
+        self.x_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
+
+        self.x_combobox = ttk.Combobox(self.option_frame, textvariable=self.selected_x, values=self.x_options)
+        self.x_combobox.grid(row=0, column=1, padx=5, pady=5)
+
+        self.y_label = ttk.Label(self.option_frame, text="Y:")
+        self.y_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.E)
+
+        self.y_combobox = ttk.Combobox(self.option_frame, textvariable=self.selected_y, values=self.y_options)
+        self.y_combobox.grid(row=1, column=1, padx=5, pady=5)
+
+        self.plot_button = ttk.Button(self.option_frame, text="Plot", command=self.plot)
+        self.plot_button.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
+
+        self.exit_button = ttk.Button(self.option_frame, text="Exit", command=sys.exit)
+        self.exit_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
+
+    def update_options(self, x_options, y_options):
+        self.x_options = x_options
+        self.y_options = y_options
+        self.x_combobox['values'] = self.x_options
+        self.y_combobox['values'] = self.y_options
+
+    def plot(self):
+        selected_x = self.selected_x.get()
+        selected_y = self.selected_y.get()
+
+        # Get the arrays of values for the selected options
+        x_values = self.myoptions[selected_x]
+        y_values = self.myoptions[selected_y]
+
+        fig, ax = plt.subplots()
+        ax.plot(x_values, y_values)
+        ax.scatter(x_values, y_values)
+        ax.grid()
+        ax.set_xlabel(selected_x)
+        ax.set_ylabel(selected_y)
+        ax.set_title(f"{selected_x} vs {selected_y}")
+        
+
+        # Avoid scientific notations
+        ax.ticklabel_format(useOffset=False, style='plain')
+
+        #window = ImageWindow(self.root, fig, f"{selected_x} vs {selected_y}")
+        plt.show()
+
---
+++ mytest/p3.py
@@ -0,0 +1,116 @@
+import tkinter as tk
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+### Exclude Dominated Point ###
+
+def exclude_dominated_points(pareto_front):
+    """Filters a list of points in the Pareto front to exclude any dominated points.
+    
+    Args:
+        pareto_front (list): A list of lists representing points in the Pareto front.
+    
+    Returns:
+        A filtered list of lists representing non-dominated points in the Pareto front.
+    """
+    filtered_pareto_front = []
+    for i, point1 in enumerate(pareto_front):
+        dominated = False
+        for j, point2 in enumerate(pareto_front):
+            if i == j:
+                continue
+            if all(p1 >= p2 for p1, p2 in zip(point1, point2)):
+                dominated = True
+                break
+        if not dominated:
+            filtered_pareto_front.append(point1)
+    return filtered_pareto_front
+
+def excluded_dataframe(df, objectives):
+    pareto_pts = df[objectives].values.tolist()
+    new_pareto_pts = exclude_dominated_points(pareto_pts)
+    updated_df = pd.DataFrame(new_pareto_pts, columns = objectives)
+    return updated_df
+
+
+def plot_2d(ax, df, label, objectives):
+
+    ax.plot(df[objectives[0]], df[objectives[1]])
+    ax.scatter(df[objectives[0]], df[objectives[1]])
+    ax.set_xlabel(objectives[0])
+    ax.set_ylabel(objectives[1])
+    ax.ticklabel_format(useOffset=False, style='plain')
+    
+    ax.grid()
+    ax.set_title(label)
+
+
+### Tk App ###
+
+class App:
+    def __init__(self, root, options, df, objectives):
+        self.root = root
+        self.options = options
+        self.selected_option = []
+        self.create_widgets()
+        self.df = df
+        self.objectives = objectives
+
+    def create_widgets(self):
+        self.option_var = tk.StringVar(value=self.options)
+        self.option_listbox = tk.Listbox(self.root, listvariable=self.option_var, selectmode=tk.EXTENDED)
+        self.option_listbox.pack(padx=10, pady=5)
+
+        #self.plot_button = tk.Button(self.root, text="Plot", command=self.plot_selected_options)
+        #self.plot_button.pack(pady=5)
+
+        self.exit_button = tk.Button(self.root, text="Exit", command=self.exit_program)
+        self.exit_button.pack(pady=5)
+
+        # Create a context menu for the option listbox
+        self.option_menu = tk.Menu(self.root, tearoff=False)
+        self.option_menu.add_command(label="Additional Info", command=self.show_additional_info)
+        self.option_listbox.bind("<Button-3>", self.show_context_menu)
+
+    def show_context_menu(self, event):
+        selection = self.option_listbox.curselection()
+        if selection:
+            self.selected_options = [self.options[index] for index in selection]  # Update selected options
+            menu_label = f"Additional Info ({len(self.selected_options)} options selected)"
+            self.option_menu.entryconfigure(0, label=menu_label)
+            if len(self.selected_options)>1:
+                self.option_menu.entryconfigure(1, label="Plot", command=self.plot_multiple_selected_options)
+            else:
+                self.option_menu.entryconfigure(1, label="Plot", command=self.plot_one_selected_option)
+            self.option_menu.tk_popup(event.x_root, event.y_root)
+
+    def show_additional_info(self):
+        for selected_option in self.selected_options:
+            print(f"Additional info for {selected_option}")
+
+    def plot_one_selected_option(self):
+
+        if self.selected_options[0] == "Pareto":
+
+            fig, ax = plt.subplots()
+            plot_2d(ax, self.df, self.selected_options[0], self.objectives)
+            plt.show()
+
+        else:
+            fig, ax = plt.subplots()
+            updated_df = excluded_dataframe(self.df, self.objectives)
+
+            plot_2d(ax, updated_df, self.selected_options[0], self.objectives)
+            plt.show()
+
+    def plot_multiple_selected_options(self):
+        fig, ax = plt.subplots()
+        plot_2d(ax, self.df, self.selected_options[0], self.objectives)
+        updated_df = excluded_dataframe(self.df, self.objectives)
+
+        plot_2d(ax, updated_df, self.selected_options[1], self.objectives)
+        plt.show()
+
+    def exit_program(self):
+        sys.exit()
